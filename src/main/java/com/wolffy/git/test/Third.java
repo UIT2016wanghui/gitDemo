d696916be278a5fb3b700e3fdeb7db3df7c75562
“wolffy” <wolffy@163.com>
2023-06-01 04:29:16 +0800

    create init 第4次提交代码

--- src/main/java/com/wolffy/git/test/Third.java
+++ src/main/java/com/wolffy/git/test/Third.java
@@ -3,5 +3,6 @@ package com.wolffy.git.test;
 public class Third {
     public static void main(String[] args) {
         System.out.println("third");
+        System.out.println("third1");
     }
 }
